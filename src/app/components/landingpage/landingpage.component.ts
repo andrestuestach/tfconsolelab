c5bb7bfd48d9a2cbaa0a6476df52cf27ff82832a
andrestuestach <andrestuesta_22@outlook.com>
2024-11-17 22:54:43 -0500

    Landing pages

---
+++ src/app/components/landingpage/landingpage.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.css'
+})
+export class LandingpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
